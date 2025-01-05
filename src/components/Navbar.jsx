382f6c183a63a94dea178b2ab13b5d8ef1b3b6c4
Ajay Chakravarthi <141811031+Ajaychaki2004@users.noreply.github.com>
2025-01-05 23:47:26 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,3 @@
-// import { FaLinkedin } from "react-icons/fa";
-// import { FaInstagram } from "react-icons/fa";
-// import { FaGithub } from "react-icons/fa";
-
-
-// const Navbar = () => {
-//     return <nav className="mb-20 flex items-center justify-between py-6">
-//         <div className="flex  flex-shrink -0 items-center">
-//         </div>
-//             <div className="m-8 flex itemcenter justify-center gap-4 txt-2xl">
-//                 <FaLinkedin />
-//                 <FaGithub />
-//                 <FaInstagram />
-//             </div>
-//     </nav>
-// }
-
-// export default Navbar;
-
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
